dd75ccf3626033bcd98799e308278a4a1b3e5aab
Marcel Sprada <msprada@web.de>
2026-01-16 10:58:44 +0100

    - implement base entity to use generics

---
+++ caseStudy.RoomBooking.Domain/Entities/Base.cs
@@ -0,0 +1,7 @@
+namespace caseStudy.RoomBooking.Domain.Entities
+{
+    public class BaseEntity
+    {
+         public string Id { get; set; }
+    }
+}
--- caseStudy.RoomBooking.Domain/Entities/User.cs
+++ caseStudy.RoomBooking.Domain/Entities/User.cs
@@ -1,8 +1,8 @@
 namespace caseStudy.RoomBooking.Domain.Entities
 {
-    public class User
+    public class User:BaseEntity
     {
-        public int Id { get; set; }
+      
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
