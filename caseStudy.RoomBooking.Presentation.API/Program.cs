1007dac4070de7e8bc26aa86ae2ee614d10b532b
Marcel Sprada <msprada@web.de>
2026-01-16 12:13:38 +0100

    - add controllers to minimal API

--- caseStudy.RoomBooking.Presentation.API/Program.cs
+++ caseStudy.RoomBooking.Presentation.API/Program.cs
@@ -1,6 +1,18 @@
+using System.Reflection.Metadata;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();  // Enable MVC controllers
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.MapControllers();  // Map controller routes
 
 app.Run();
