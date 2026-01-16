174137a4a70473aa9ea9513620a76eea549068b8
Marcel Sprada <msprada@web.de>
2026-01-16 18:22:49 +0100

    - use DI to connect services to application

--- caseStudy.RoomBooking.Presentation.API/Program.cs
+++ caseStudy.RoomBooking.Presentation.API/Program.cs
@@ -1,6 +1,15 @@
-using System.Reflection.Metadata;
+using caseStudy.RoomBooking.Domain.Entities;
+using caseStudy.RoomBooking.Domain.Repositories;
+using caseStudy.RoomBooking.Infrastructure.Persistence;
+using caseStudy.RoomBooking.Infrastructure;
+
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddInfrastructure(builder.Configuration);
+
+// Register repositories
+builder.Services.AddScoped<IRepository<User>, EFRepository<User>>();
 
 // Add services to the container.
 builder.Services.AddControllers();  // Enable MVC controllers
