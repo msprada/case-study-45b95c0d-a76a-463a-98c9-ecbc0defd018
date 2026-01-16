adf12c90f2db8252705af7fbfb878b174768371f
Marcel Sprada <msprada@web.de>
2026-01-16 10:58:18 +0100

    - remove additional entities yagnin

--- caseStudy.RoomBooking.Infrastructure/Persistence/Entities/User.cs
+++
@@ -1,9 +0,0 @@
-namespace caseStudy.RoomBooking.Infrastructure.Persistence.Entities
-{
-      public class User:BaseEntity
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-    }
-}
