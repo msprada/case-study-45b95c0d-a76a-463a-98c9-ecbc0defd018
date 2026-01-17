204a24c31eeaa1892129d119b2dbceb64acac73f
Marcel Sprada <msprada@web.de>
2026-01-17 19:09:44 +0100

    - provide implementation details

---
+++ caseStudy.RoomBooking.Application/BookingService.cs
@@ -0,0 +1,49 @@
+using caseStudy.RoomBooking.Domain.Entities;
+using caseStudy.RoomBooking.Domain.Repositories;
+using caseStudy.RoomBooking.Application.Abstractions;
+
+public class BookingService:IBookingService
+{
+    private readonly IRepository<Booking> _bookingRepository;
+
+    public BookingService(IRepository<Booking> bookingRepository)
+    {
+        _bookingRepository = bookingRepository;
+    }
+
+    public async Task<Booking> CreateBookingAsync(BookingCreateDto request)
+    {
+        var booking = new Booking
+        {
+            RoomId = request.RoomId,
+            UserId = request.UserId,
+            Starting = request.Starting,
+            Ending = request.Ending
+        };
+
+        await _bookingRepository.Add(booking);
+        return booking;
+    }
+
+    public async Task<Booking> GetBookingByIdAsync(Guid bookingId)
+    {
+        return await _bookingRepository.GetById(bookingId.ToString());
+    }
+
+    public async Task<IEnumerable<Booking>> GetAllBookingsAsync()
+    {
+        return await _bookingRepository.GetAll();
+    }
+
+    public async Task<bool> CancelBookingAsync(Guid bookingId)
+    {
+        var booking = await _bookingRepository.GetById(bookingId.ToString());
+        if (booking == null)
+        {
+            return false;
+        }
+
+        await _bookingRepository.Delete(booking);
+        return true;
+    }
+}
