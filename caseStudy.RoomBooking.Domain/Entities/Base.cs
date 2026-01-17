9eb596c05711e2bf7bbe4bae204929068da0aecd
Marcel Sprada <msprada@web.de>
2026-01-17 18:24:37 +0100

    - add two more props

--- caseStudy.RoomBooking.Domain/Entities/Base.cs
+++ caseStudy.RoomBooking.Domain/Entities/Base.cs
@@ -3,5 +3,8 @@ namespace caseStudy.RoomBooking.Domain.Entities
     public class BaseEntity
     {
          public string Id { get; set; }
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
     }
 }
